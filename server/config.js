cccb2175e36554aad4d746f451dd7006343dd413
Sanjeeb Sapkota <hackingmindsam@hotmail.com>
2020-02-22 12:10:44 +0545

    removed token

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'DATABASE': 'mongodb:localhost:27017/faceRecognition',
 	'SECRET':	'thisisasupersecretpasswordthingy',
-    'FACE_API_KEY': 'ebb7dee6d1814347ab00b43e738d9d76',
+    'FACE_API_KEY': '', //place your key here
     'FACE_API_HOST': 'centralus.api.cognitive.microsoft.com',
     'FACE_API_PATH_VERIFY': '/face/v1.0/verify',
     'FACE_API_PATH_DETECT': '/face/v1.0/detect?returnFaceId=true',
